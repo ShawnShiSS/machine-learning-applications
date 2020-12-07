5e7e18e4741ed87484d686ac016ae1571840245c
Shawn Shi <ss.shawnshi@gmail.com>
2020-12-06 22:57:28 -0500

    Add datasets and ML.NET Model Builder project

---
+++ src/MLApplications.MLNetModelBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLApplications.MLNetModelBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a placeholder for ML.NET Model Builder");
+        }
+    }
+}
