10ecb476967b1577dbf21a6f50040a8ae11b65e9
Shawn Shi <ss.shawnshi@gmail.com>
2020-12-07 22:41:43 -0500

    Add extensions to auto create database

---
+++ src/MLApplications.Infrastructure/Extensions/IApplicationBuilderExtensions.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using MLApplications.Core.Interfaces;
+using MLApplications.Core.Specifications;
+using MLApplications.Infrastructure.Data;
+
+namespace MLApplications.Infrastructure.Extensions
+{
+    public static class IApplicationBuilderExtensions
+    {
+        /// <summary>
+        ///     Create Cosmos DB if not exist
+        /// </summary>
+        /// <param name="builder"></param>
+        public static void EnsureCosmosDbIsCreated(this IApplicationBuilder builder)
+        {
+            using (var serviceScope = builder.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var factory = serviceScope.ServiceProvider.GetService<ICosmosDbContainerFactory>();
+
+                factory.EnsureDbSetupAsync().Wait();
+            
+            }
+        }
+
+        /// <summary>
+        ///     Seed application data in Cosmos DB
+        /// </summary>
+        /// <param name="builder"></param>
+        public static void SeedApplicationData(this IApplicationBuilder builder)
+        {
+            using (var serviceScope = builder.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var webCommentRepository = serviceScope.ServiceProvider.GetService<IWebCommentRepository>();
+                var countSpecification = new WebCommentSearchAggregationSpecification("");
+                var count = webCommentRepository.GetItemsCountAsync(countSpecification).Result;
+
+                if(count == 0)
+                {
+                    foreach(var webComment in MLApplications.Infrastructure.Data.SeedData.WebComments)
+                    {
+                        // NOTE: must Wait() here
+                        webCommentRepository.AddItemAsync(webComment).Wait();
+                    }
+                }
+
+            }
+        }
+        
+    }
+}
---
+++ src/MLApplications.Infrastructure/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using MLApplications.Infrastructure.CosmosDbData.Config;
+using MLApplications.Infrastructure.Data;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+
+namespace MLApplications.Infrastructure.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        ///     Register a singleton instance of Cosmos Db Container Factory, which is a wrapper for the CosmosClient.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="endpointUrl"></param>
+        /// <param name="primaryKey"></param>
+        /// <param name="databaseName"></param>
+        /// <param name="containers"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddCosmosDb(this IServiceCollection services,
+                                                     string endpointUrl,
+                                                     string primaryKey,
+                                                     string databaseName,
+                                                     List<ContainerInfo> containers)
+        {
+            Microsoft.Azure.Cosmos.CosmosClient client = new Microsoft.Azure.Cosmos.CosmosClient(endpointUrl, primaryKey);
+            var cosmosDbClientFactory = new CosmosDbContainerFactory(client, databaseName, containers);
+
+            // This will be done at the API level so we ONLY ensure db creation in development environment
+            // cosmosDbClientFactory.EnsureDbSetupAsync().Wait();
+
+            services.AddSingleton<ICosmosDbContainerFactory>(cosmosDbClientFactory);
+
+            return services;
+        }
+
+    }
+}
