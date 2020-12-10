2d91f2454d5a2c4dec689338b02167568bd4b8cd
Shawn Shi <ss.shawnshi@gmail.com>
2020-12-09 23:24:18 -0500

    API: Setup Swagger

---
+++ src/MLApplications.API/Config/SwaggerConfig.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NSwag;
+using NSwag.Generation.Processors.Security;
+using ZymLabs.NSwag.FluentValidation;
+
+namespace MLApplications.API.Config
+{
+    /// <summary>
+    ///     Swagger
+    /// </summary>
+    public static class SwaggerConfig
+    {
+        /// <summary>
+        ///     NSwag Swagger config
+        /// </summary>
+        /// <param name="services"></param>
+        public static void SetupNSwag(this IServiceCollection services)
+        {
+            // Register the Swagger services
+            services.AddOpenApiDocument((options, serviceProvider) => {
+                options.DocumentName = "v1";
+                options.Title = "MLApplications API";
+                options.Version = "v1";
+
+                // FluentValidation, add the fluent validations schema processor
+                var fluentValidationSchemaProcessor = serviceProvider.GetService<FluentValidationSchemaProcessor>();
+                options.SchemaProcessors.Add(fluentValidationSchemaProcessor);
+
+                options.OperationProcessors.Add(new OperationSecurityScopeProcessor("auth"));
+                options.DocumentProcessors.Add(new SecurityDefinitionAppender("auth", new OpenApiSecurityScheme
+                {
+                    Type = OpenApiSecuritySchemeType.Http,
+                    In = OpenApiSecurityApiKeyLocation.Header,
+                    Scheme = "bearer",
+                    BearerFormat = "jwt"
+                }));
+            });
+
+            // Add the FluentValidationSchemaProcessor as a singleton
+            services.AddSingleton<FluentValidationSchemaProcessor>();
+        }
+    }
+}
