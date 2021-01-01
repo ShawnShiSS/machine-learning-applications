25ab71b916438a23d31bac29ff9ef350f8744c29
Shawn Shi <ss.shawnshi@gmail.com>
2021-01-01 13:48:36 -0500

    Add new project for anomaly detection

---
+++ src/MLApplications.AnomalyDetection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLApplications.AnomalyDetection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
